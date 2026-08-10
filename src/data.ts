297955de5675fe83740bc9616654c22197f49e29
Ghanmidev2012 <242857573+Ghanmidev2012@users.noreply.github.com>
2026-08-10 13:35:45 +0100

    Add files via upload

---
+++ src/data.ts
@@ -0,0 +1,17 @@
+import { data } from './datatype';
+
+export const datatype: data[] = [
+    {
+        id: 1,
+        name: "adam",
+        age: 15,
+    },
+    {
+        id: 2,
+        name: "jad",
+        age: 10,
+
+    }
+
+    
+];
